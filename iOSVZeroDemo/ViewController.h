275ae129b9979c9aaa0905540c85523563254109
Jeff Prestes <jeffprestes@gmail.com>
2015-06-25 22:53:29 -0500

    Initial Commit

---
+++ iOSVZeroDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSVZeroDemo
+//
+//  Created by Prestes, Jefferson on 6/25/15.
+//  Copyright (c) 2015 Prestes, Jefferson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSVZeroDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSVZeroDemo
+//
+//  Created by Prestes, Jefferson on 6/25/15.
+//  Copyright (c) 2015 Prestes, Jefferson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
