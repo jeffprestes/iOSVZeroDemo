cd0f09110d19ee9bf0f2c97aa074ecf21b3be963
Jeff Prestes <jeffprestes@gmail.com>
2015-07-27 21:56:03 -0400

    New basic iOS Project

---
+++ iOSVZeroDemo/iOSVZeroDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSVZeroDemo
+//
+//  Created by Prestes, Jefferson on 7/27/15.
+//  Copyright (c) 2015 Prestes, Jefferson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
