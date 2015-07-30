ba07d74061c32b6d099361a3665a6fa5786a50c9
Jeff Prestes <jeffprestes@gmail.com>
2015-07-30 18:13:13 -0400

    Created call to Server Side to get Braintree Token

--- iOSVZeroDemo/iOSVZeroDemo/ViewController.m
+++ iOSVZeroDemo/iOSVZeroDemo/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <BTDropInViewControllerDelegate>
 
 @end
 
@@ -16,12 +16,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self getBraintreeToken];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma GetToken
+
+- (void) getBraintreeToken    {
+    
+    NSURL *clientTokenServerURL = [NSURL URLWithString:@"https://www.novatrix.com.br/bhdemo/token.php"];
+    NSMutableURLRequest *clientTokenRequest = [NSMutableURLRequest requestWithURL:clientTokenServerURL];
+    [clientTokenRequest setValue:@"text/plain" forHTTPHeaderField:@"Accept"];
+    
+    NSLog(@"Initializing call to server side to get Braintree Token");
+    
+    [NSURLConnection sendAsynchronousRequest:clientTokenRequest
+                                       queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                               
+                               self.clientToken = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                               NSLog(@"Token obtained: %@", self.clientToken);
+                               
+                               self.btnPay.enabled = TRUE;
+                               NSLog(@"Button Pay is enabled");
+                           }];
+}
+
+#pragma DropIn Call
+- (void) callDropIn:(id)sender  {
+    
+}
+
+- (void) dropInViewControllerDidCancel:(BTDropInViewController *)viewController     {
+    
+}
+
+- (void) dropInViewController:(BTDropInViewController *)viewController didSucceedWithPaymentMethod:(BTPaymentMethod *)paymentMethod     {
+    
 }
 
 @end
